70dfc482c9a3383d8cd831cd9e40be0baea29554
tazlauanubianca <tazlauanubianca@yahoo.com>
2020-04-24 16:11:21 +0300

    Continuous calling of the function + storing of inputs/crashes

--- fuzz.py
+++ fuzz.py
@@ -1,26 +1,23 @@
 import ctypes
 import random
+import sys
+from datetime import datetime
 
-def generate(max_length=1000000000, char_start=1, char_range=255):
-    """A string of up to `max_length` characters
-       in the range [`char_start`, `char_start` + `char_range`]"""
-    #string_length = random.randrange(0, max_length + 1)
-    string_length = max_length
-    print(string_length)
+def generate(max_length=1000000, char_start=1, char_range=255):
+    string_length = random.randrange(0, max_length + 1)
     out = ""
 
     for i in range(0, string_length):
         out += chr(random.randrange(char_start, char_start + char_range))
 
-    print(len(out))
     return out
 
 if __name__ == '__main__':
-    libPath = '/home/bianca/Desktop/thesis/Basic-Fuzzer/out-rdynamic'
+    lib_path = '/home/bianca/Desktop/thesis/Basic-Fuzzer/out-rdynamic'
     lib = None
 
     try:
-        lib = ctypes.CDLL(libPath)
+        lib = ctypes.CDLL(lib_path)
     except OSError as e:
         print('loading out-rdynamic failed: %s', e)
 
@@ -28,9 +25,20 @@ if __name__ == '__main__':
         print('no out-rdynamic found')
 
     lib.strlen.argtypes = [ctypes.c_char_p]
-    inputString = generate()
-    #print(inputString)
-    result = lib.strlen(inputString)
+    
+    while True:
+        input_string = generate()
+        print("[{}] Test with input size {} ".format(datetime.now(), len(input_string)))
+        try:
+            f_in = open("inputs.txt", "a")
+            f_in.write("Test input [{}]: {}".format(datetime.now(), input_string))
+            f_in.close()
+            
+            result = lib.strlen(input_string)
 
-    print("Result is {}".format(int(result)))
+        except:
+            f = open("crashes.txt", "a")
+            f.write("Crash number [{}]: {}".format(datetime.now(), input_string))
+            f.close()
 
+            print("[{}] Crash occured for input with size {}".format(datetime.now(), len(input_string))) 
